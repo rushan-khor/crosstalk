8664d880f9ac51930550bdccfdd21913ada0b4d4
Ru Shan Khor <rushan.khor@gmail.com>
2019-03-05 18:09:35 -0500

    Fix admin functions

--- admin_functions.py
+++ admin_functions.py
@@ -11,22 +11,25 @@ WEBHOOK_SECRET_KEY = os.environ['TELEGRAM_WEBHOOK_KEY']
 WEBHOOK_URL = os.environ['TELEGRAM_WEBHOOK_HOSTNAME'] + WEBHOOK_SECRET_KEY
 
 CONSOLE_TITLE_DIVIDER = '\n'
-CONSOLE_BULLET = '*'
+CONSOLE_LINE_INDENT = '* '
 
 
 def set_webhook():
     r = requests.get(TELEGRAM_API_URL + 'setWebhook', json={'url': WEBHOOK_URL})
-    return r.json()
+    result = r.json()
+    return str(result)
 
 
 def delete_webhook():
     r = requests.get(TELEGRAM_API_URL + 'deleteWebhook')
-    return r.json()
+    result = r.json()
+    return str(result)
 
 
 def get_webhook_info():
     r = requests.get(TELEGRAM_API_URL + 'getWebhookInfo')
-    return r.json()
+    result = r.json()
+    return str(result)
 
 
 def generate_secret_key():
@@ -38,13 +41,13 @@ def generate_secret_key():
 
 if __name__ == '__main__':
     print(CONSOLE_TITLE_DIVIDER + 'TELEGRAM BOT ADMIN')
-    print(CONSOLE_BULLET, 'Telegram bot token:\t', TOKEN)
-    print(CONSOLE_BULLET, 'Telegram API URL:\t\t', TELEGRAM_API_URL)
+    print(CONSOLE_LINE_INDENT + 'Telegram bot token:\t' + TOKEN)
+    print(CONSOLE_LINE_INDENT + 'Telegram API URL:\t\t' + TELEGRAM_API_URL)
 
     print(CONSOLE_TITLE_DIVIDER + 'WEBHOOK ADMIN')
-    print(CONSOLE_BULLET, 'Webhook secret key:\t\t', WEBHOOK_SECRET_KEY)
-    print(CONSOLE_BULLET, 'Webhook URL:\t\t\t\t', WEBHOOK_URL)
-    # print(CONSOLE_BULLET, 'Set webhook response:\t\t', set_webhook())
-    # print(CONSOLE_BULLET, 'Delete webhook response:\t', delete_webhook())
-    print(CONSOLE_BULLET, 'Webhook info response:\t', get_webhook_info())
-    # print(CONSOLE_BULLET, 'New key generated:\t\t', generate_secret_key())
+    print(CONSOLE_LINE_INDENT + 'Webhook secret key:\t\t' + WEBHOOK_SECRET_KEY)
+    print(CONSOLE_LINE_INDENT + 'Webhook URL:\t\t\t\t' + WEBHOOK_URL)
+    # print(CONSOLE_LINE_INDENT + 'Set Webhook response:\t\t' + set_webhook())
+    # print(CONSOLE_LINE_INDENT + 'Delete Debhook response:\t' + delete_webhook())
+    print(CONSOLE_LINE_INDENT + 'Webhook Info response:\t' + get_webhook_info())
+    # print(CONSOLE_LINE_INDENT + 'New key generated:\t\t' + generate_secret_key())
