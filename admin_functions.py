40513abd8620b7ffb6ba9d3954c6c5d36bd7f40c
Ru Shan Khor <rushan.khor@gmail.com>
2019-03-05 14:41:37 -0500

    Refactor

--- admin_functions.py
+++ admin_functions.py
@@ -15,12 +15,17 @@ CONSOLE_BULLET = '*'
 
 
 def set_webhook():
-    r = requests.post(TELEGRAM_API_URL + 'setWebhook', data={'url': WEBHOOK_URL})
+    r = requests.get(TELEGRAM_API_URL + 'setWebhook', data={'url': WEBHOOK_URL})
     return r.json()
 
 
 def delete_webhook():
-    r = requests.post(TELEGRAM_API_URL + 'setWebhook', data={'url': ''})
+    r = requests.get(TELEGRAM_API_URL + 'deleteWebhook')
+    return r.json()
+
+
+def get_webhook_info():
+    r = requests.get(TELEGRAM_API_URL + 'getWebhookInfo')
     return r.json()
 
 
@@ -37,8 +42,9 @@ if __name__ == '__main__':
     print(CONSOLE_BULLET, 'Telegram API URL:\t\t', TELEGRAM_API_URL)
 
     print(CONSOLE_TITLE_DIVIDER + 'WEBHOOK ADMIN')
-    print(CONSOLE_BULLET, 'Webhook secret key:\t', WEBHOOK_SECRET_KEY)
-    print(CONSOLE_BULLET, 'Webhook URL:\t\t\t', WEBHOOK_URL)
+    print(CONSOLE_BULLET, 'Webhook secret key:\t\t', WEBHOOK_SECRET_KEY)
+    print(CONSOLE_BULLET, 'Webhook URL:\t\t\t\t', WEBHOOK_URL)
     # print(CONSOLE_BULLET, 'Set webhook response:\t\t', set_webhook())
     # print(CONSOLE_BULLET, 'Delete webhook response:\t', delete_webhook())
-    # print(CONSOLE_BULLET, 'New key generated:\t\t', generate_secret_key())
+    print(CONSOLE_BULLET, 'Webhook info response:\t', get_webhook_info())
+    print(CONSOLE_BULLET, 'New key generated:\t\t', generate_secret_key())
