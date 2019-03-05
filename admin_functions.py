49ad2f890dc43af79c22ae4976fbcb453f500b18
Ru Shan Khor <rushan.khor@gmail.com>
2019-03-05 13:55:39 -0500

    Refactor admin functions

--- admin_functions.py
+++ admin_functions.py
@@ -10,6 +10,7 @@ TELEGRAM_API_URL = f'https://api.telegram.org/bot{TOKEN}/'
 WEBHOOK_SECRET_KEY = os.environ['WEBHOOK_SECRET_KEY']
 WEBHOOK_URL = os.environ['WEBHOOK_HOSTNAME'] + WEBHOOK_SECRET_KEY
 
+CONSOLE_TITLE_DIVIDER = '\n'
 CONSOLE_BULLET = '*'
 
 
@@ -31,13 +32,13 @@ def generate_secret_key():
 
 
 if __name__ == '__main__':
-    print('\nTELEGRAM ADMIN')
+    print(CONSOLE_TITLE_DIVIDER + 'TELEGRAM ADMIN')
     print(CONSOLE_BULLET, 'Telegram bot token:\t', TOKEN)
     print(CONSOLE_BULLET, 'Telegram API URL:\t\t', TELEGRAM_API_URL)
     print(CONSOLE_BULLET, 'Webhook secret key:\t', WEBHOOK_SECRET_KEY)
     print(CONSOLE_BULLET, 'Webhook URL:\t\t\t', WEBHOOK_URL)
 
-    print('\nWEBHOOK ADMIN')
+    print(CONSOLE_TITLE_DIVIDER + 'WEBHOOK ADMIN')
     # print(CONSOLE_BULLET, 'Set webhook response:\t\t', set_webhook())
     # print(CONSOLE_BULLET, 'Delete webhook response:\t', delete_webhook())
     # print(CONSOLE_BULLET, 'New key generated:\t\t', generate_secret_key())
