8f684e5e5747e6236f24e13d26d552f35886786f
Ru Shan Khor <rushan.khor@gmail.com>
2019-03-06 15:52:56 -0500

    Refactor to use edited_mssage flag

--- flask_server.py
+++ flask_server.py
@@ -3,7 +3,7 @@ from os import environ
 from flask import Flask, jsonify, request
 from werkzeug import http, exceptions
 
-from crosstalk_telegram_bot import handle_message, handle_edited_message
+from crosstalk_telegram_bot import handle_message
 
 WEBHOOK_SECRET_KEY = environ['TELEGRAM_WEBHOOK_KEY']
 
@@ -20,11 +20,11 @@ def handle_webhook():
     return jsonify({'ok': True})
 
 
-def handle_update(update):
+def handle_update(update=None):
     if 'edited_message' in update:
-        handle_edited_message(edited_message=update['edited_message'])
+        handle_message(message=update['message'], is_edited=True)
     elif 'message' in update:
-        handle_message(message=update['message'])
+        handle_message(message=update['message'], is_edited=False)
 
     raise KeyError(f'No message found in Telegram update {update}.')
 
