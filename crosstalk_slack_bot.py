fc0a3935ecfc344d4c0a803600ebed763efbd475
Ru Shan Khor <rushan.khor@gmail.com>
2019-03-05 19:27:44 -0500

    Add nametag

--- crosstalk_slack_bot.py
+++ crosstalk_slack_bot.py
@@ -5,5 +5,26 @@ import requests
 SLACK_BOT_WEBHOOK = os.environ['SLACK_BOT_WEBHOOK']
 
 
-def send_message(text):
-    r = requests.post(SLACK_BOT_WEBHOOK, json={'text': text})  # r.text will be 'ok'
+def send_message(from_user, text):
+    payload = {
+        "text": text,
+        "blocks": [
+            {
+                "type": "context",
+                "elements": [
+                    {
+                        "type": "mrkdwn",
+                        "text": f":speech_balloon: _{from_user} says:_"
+                    }
+                ]
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": text,
+                }
+            },
+        ]
+    }
+    r = requests.post(SLACK_BOT_WEBHOOK, json=payload)  # r.text will be 'ok'
--- crosstalk_telegram_bot.py
+++ crosstalk_telegram_bot.py
@@ -2,4 +2,7 @@ from crosstalk_slack_bot import send_message
 
 
 def handle_update(update):
-    send_message(update['message']['text'])
+    message = update['message'] or update['edited_message']
+    text = message['text']
+    user = message['from']['first_name']
+    send_message(from_user=user, text=text)
