1f1ea2b7eff76726b1377feaa47654d9b4890fc1
Ru Shan Khor <rushan.khor@gmail.com>
2019-03-06 20:37:41 -0500

    Fix message content types

--- crosstalk_telegram_bot.py
+++ crosstalk_telegram_bot.py
@@ -20,7 +20,7 @@ def handle_message(message, is_edited=False):
         raise KeyError(f'No from key found in Telegram message {message}.')
 
     user_intent = get_user_intent(message=message, is_edited=is_edited)
-    message_text = get_message_text_or_caption(message)
+    message_text = get_message_text(message)
 
     thumbnail_url = message_thumbnail_url(message)
     download_url = message_download_button_url(message)
@@ -33,52 +33,97 @@ def handle_edited_message(message):
 
 
 def get_user_intent(message, is_edited=False):
-    from_user = message['from']['first_name']
-
     if is_edited:
-        return f':pencil2: _{from_user} edited a message to say_:'
+        return get_edited_message_user_intent(message)
     elif 'reply_to_message' in message:
-        preceding_message = message['reply_to_message']
+        return get_reply_message_user_intent(message)
+    elif 'forward_from' in message:
+        return get_forwarded_message_user_intent(message)
+    else:
+        return get_original_message_user_intent(message)
 
-        preceding_user = preceding_message['from'].get('first_name', 'an unknown user')
 
-        if preceding_user == from_user:
-            preceding_user = 'their own'
-        else:
-            preceding_user += '\'s'
+def get_edited_message_user_intent(message):
+    from_user = message['from']['first_name']
+    return f':pencil2: _{from_user} edited a message to say_:'
 
-        preceding_message_type = get_message_content_type(preceding_message).replace('_', ' ')
 
-        if preceding_message_type == 'sticker':
-            preceding_text = preceding_message['sticker']['emoji']
-        else:
-            preceding_text = get_message_text_or_caption(preceding_message)
+def get_reply_message_user_intent(message):
+    preceding_message = message['reply_to_message']
 
-        if len(preceding_text) > 20:
-            preceding_text = preceding_text[0:19] + '...'
+    from_user = message['from']['first_name']
+    preceding_user = preceding_message['from'].get('first_name', 'an unknown user')
+
+    if preceding_user == from_user:
+        preceding_user = 'their own'
+    else:
+        preceding_user += '\'s'
+
+    preceding_message_type = get_pretty_message_content_type(preceding_message)
+
+    if preceding_message_type == 'sticker':
+        preceding_text = preceding_message['sticker']['emoji']
+    else:
+        preceding_text = get_message_text_or_caption(preceding_message)
 
         if not preceding_text:
             return (f':mailbox: _{from_user} '
                     f'replied to {preceding_user} {preceding_message_type}_:')
-        else:
-            return (f':mailbox: _{from_user} '
-                    f'replied to {preceding_user} {preceding_message_type} '
-                    f'"{preceding_text}"_:')
-    elif 'forward_from' in message:
-        preceding_user = message['forward_from'].get('first_name', 'an unknown user')
+        elif len(preceding_text) > 20:
+            preceding_text = preceding_text[0:19] + '...'
 
-        if preceding_user == from_user:
-            preceding_user = 'their own'
-        else:
-            preceding_user += '\'s'
+    return (f':mailbox: _{from_user} '
+            f'replied to {preceding_user} {preceding_message_type} '
+            f'"{preceding_text}"_:')
 
-        return f':arrow_right: _{from_user} forwarded {preceding_user} message_:'
+
+def get_forwarded_message_user_intent(message):
+    from_user = message['from']['first_name']
+    preceding_user = message['forward_from'].get('first_name', 'an unknown user')
+
+    if preceding_user == from_user:
+        preceding_user = 'their own'
     else:
+        preceding_user += '\'s'
+
+    return f':arrow_right: _{from_user} forwarded {preceding_user} message_:'
+
+
+def get_original_message_user_intent(message):
+    from_user = message['from']['first_name']
+    message_type = get_pretty_message_content_type(message)
+
+    if message_type == 'message':
         return f':speech_balloon: _{from_user} says_:'
+    elif message_type == 'photo':
+        return f':cityscape: _{from_user} sent a {message_type}_:'
+    elif message_type == 'audio clip' or message_type == 'voice recording':
+        return f':microphone: _{from_user} sent a {message_type}_:'
+    elif message_type == 'video' or message_type == 'video note':
+        return f':clapper: _{from_user} sent a {message_type}_:'
+    elif message_type == 'sticker':
+        return f':fire: _{from_user} sent a {message_type}_:'
+    else:
+        return f':page_facing_up: _{from_user} sent a {message_type}_:'
+
+
+def get_message_text(message):
+    if get_message_content_type(message) == 'sticker':
+        return message['sticker']['emoji']
+
+    return get_message_text_or_caption(message)
 
 
 def get_message_text_or_caption(message):
     return message.get('text') or message.get('caption')
+
+
+def message_is_single_photo_type(message):
+    try:
+        photo_mime_type_prefix = 'image/'
+        return photo_mime_type_prefix in message['document']['mime_type']
+    except KeyError:
+        return False
 
 
 def message_thumbnail_url(message):
@@ -117,6 +162,19 @@ def get_url_from_file_id(file_id):
         raise KeyError(f'No file path provided for file ID: {file_id}.')
 
     return TELEGRAM_API_BASE_FILE_URL + file_path
+
+
+def get_pretty_message_content_type(message):
+    message_type = get_message_content_type(message)
+
+    if message_type == 'audio':
+        return 'audio clip'
+    elif message_type == 'voice':
+        return 'voice recording'
+    elif message_is_single_photo_type(message):
+        return 'photo'
+
+    return message_type.replace('_', ' ')
 
 
 def get_message_content_type(message):
