28ba76e00d727df2a817e7b5187cfba7e06507d9
Ru Shan Khor <rushan.khor@gmail.com>
2019-03-08 18:16:43 -0500

    Update .env variable names

--- admin_functions.py
+++ admin_functions.py
@@ -4,30 +4,31 @@ import string
 
 import requests
 
-TOKEN = os.environ['TELEGRAM_BOT_TOKEN']
-TELEGRAM_API_URL = f'https://api.telegram.org/bot{TOKEN}/'
+BOT_TOKEN = os.environ['TELEGRAM_BOT_TOKEN']
+TELEGRAM_API_HOSTNAME = os.environ['TELEGRAM_API_HOSTNAME']
+TELEGRAM_BOT_API_URL = f'{TELEGRAM_API_HOSTNAME}bot{BOT_TOKEN}/'
 
-WEBHOOK_SECRET_KEY = os.environ['TELEGRAM_WEBHOOK_KEY']
-WEBHOOK_URL = os.environ['TELEGRAM_WEBHOOK_HOSTNAME'] + WEBHOOK_SECRET_KEY
+TELEGRAM_WEBHOOK_SECRET = os.environ['TELEGRAM_WEBHOOK_SECRET']
+TELEGRAM_WEBHOOK_URL = os.environ['TELEGRAM_WEBHOOK_HOSTNAME'] + TELEGRAM_WEBHOOK_SECRET
 
 CONSOLE_TITLE_DIVIDER = '\n'
 CONSOLE_LINE_INDENT = '* '
 
 
 def set_webhook():
-    r = requests.get(TELEGRAM_API_URL + 'setWebhook', json={'url': WEBHOOK_URL})
+    r = requests.post(TELEGRAM_BOT_API_URL + 'setWebhook', json={'url': TELEGRAM_WEBHOOK_URL})
     result = r.json()
     return str(result)
 
 
 def delete_webhook():
-    r = requests.get(TELEGRAM_API_URL + 'deleteWebhook')
+    r = requests.get(TELEGRAM_BOT_API_URL + 'deleteWebhook')
     result = r.json()
     return str(result)
 
 
 def get_webhook_info():
-    r = requests.get(TELEGRAM_API_URL + 'getWebhookInfo')
+    r = requests.get(TELEGRAM_BOT_API_URL + 'getWebhookInfo')
     result = r.json()
     return str(result)
 
@@ -41,12 +42,12 @@ def generate_secret_key():
 
 if __name__ == '__main__':
     print(CONSOLE_TITLE_DIVIDER + 'TELEGRAM BOT ADMIN')
-    print(CONSOLE_LINE_INDENT + 'Telegram bot token:\t' + TOKEN)
-    print(CONSOLE_LINE_INDENT + 'Telegram API URL:\t\t' + TELEGRAM_API_URL)
+    print(CONSOLE_LINE_INDENT + 'Telegram bot token:\t' + BOT_TOKEN)
+    print(CONSOLE_LINE_INDENT + 'Telegram API URL:\t\t' + TELEGRAM_BOT_API_URL)
 
     print(CONSOLE_TITLE_DIVIDER + 'WEBHOOK ADMIN')
-    print(CONSOLE_LINE_INDENT + 'Webhook secret key:\t\t' + WEBHOOK_SECRET_KEY)
-    print(CONSOLE_LINE_INDENT + 'Webhook URL:\t\t\t\t' + WEBHOOK_URL)
+    print(CONSOLE_LINE_INDENT + 'Webhook secret key:\t\t' + TELEGRAM_WEBHOOK_URL)
+    print(CONSOLE_LINE_INDENT + 'Webhook URL:\t\t\t\t' + TELEGRAM_WEBHOOK_URL)
     # print(CONSOLE_LINE_INDENT + 'Set Webhook response:\t\t' + set_webhook())
     # print(CONSOLE_LINE_INDENT + 'Delete Debhook response:\t' + delete_webhook())
     print(CONSOLE_LINE_INDENT + 'Webhook Info response:\t' + get_webhook_info())
